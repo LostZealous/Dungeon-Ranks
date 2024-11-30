8668c334a4b03fc20defac323afb984eb4126ce7
LostZealous <158816781+LostZealous@users.noreply.github.com>
2024-11-30 11:47:53 -0600

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,10 +1,11 @@
 // battle.js
 
 class Battle {
-    constructor(player, enemies) {
+    constructor(player, enemy) {
         this.player = player;
-        this.enemies = enemies;
+        this.enemy = enemy;
         this.battleLog = [];
+        this.currentTurn = "player"; // Tracks whose turn it is
     }
 
     rollDice(sides) {
@@ -13,63 +14,39 @@ class Battle {
 
     playerAttack() {
         const attackRoll = this.rollDice(20); // Roll a d20 for attack
-        if (attackRoll > this.enemies.armorClass) {
+        if (attackRoll > this.enemy.armorClass) {
             const damage = this.rollDice(this.player.weapon.damageDice);
-            this.enemies.currentHP -= damage;
-            this.battleLog.push(`You hit the ${this.enemies.name} for ${damage} damage!`);
+            this.enemy.currentHP -= damage;
+            this.battleLog.push(`You hit the ${this.enemy.name} for ${damage} damage!`);
         } else {
-            this.battleLog.push(`You missed the ${this.enemies.name}!`);
+            this.battleLog.push(`You missed the ${this.enemy.name}!`);
         }
+        this.currentTurn = "enemy"; // Switch turn
     }
 
     enemyAttack() {
         const attackRoll = this.rollDice(20); // Roll a d20 for attack
         if (attackRoll > this.player.armorClass) {
-            const damage = this.rollDice(this.enemies.weapon.damageDice);
+            const damage = this.rollDice(this.enemy.weapon.damageDice);
             this.player.currentHP -= damage;
-            this.battleLog.push(`The ${this.enemies.name} hits you for ${damage} damage!`);
+            this.battleLog.push(`The ${this.enemy.name} hits you for ${damage} damage!`);
         } else {
-            this.battleLog.push(`The ${this.enemies.name} missed you!`);
+            this.battleLog.push(`The ${this.enemy.name} missed you!`);
         }
+        this.currentTurn = "player"; // Switch turn
     }
 
-    startBattle() {
-        this.battleLog.push("The battle begins!");
-
-        while (this.player.currentHP > 0 && this.enemies.currentHP > 0) {
-            this.playerAttack();
-            if (this.enemies.currentHP <= 0) {
-                this.battleLog.push(`You have defeated the ${this.enemies.name}!`);
-                break;
-            }
-
-            this.enemyAttack();
-            if (this.player.currentHP <= 0) {
-                this.battleLog.push("You have been defeated!");
-                break;
-            }
+    isBattleOver() {
+        if (this.player.currentHP <= 0) {
+            this.battleLog.push("You have been defeated!");
+            return true;
         }
-
-        return this.battleLog;
+        if (this.enemy.currentHP <= 0) {
+            this.battleLog.push(`You have defeated the ${this.enemy.name}!`);
+            return true;
+        }
+        return false;
     }
 }
-
-// Example Usage
-const player = {
-    name: "Adventurer",
-    currentHP: 20,
-    armorClass: 15,
-    weapon: { name: "Sword", damageDice: 8 } // d8 damage
-};
-
-const goblin = {
-    name: "Goblin",
-    currentHP: 10,
-    armorClass: 12,
-    weapon: { name: "Dagger", damageDice: 6 } // d6 damage
-};
-
-const battle = new Battle(player, goblin);
-console.log(battle.startBattle());
 
 export { Battle };
