606900b0ab5ed1b2c8383bc26d51b05387173f61
LostZealous <158816781+LostZealous@users.noreply.github.com>
2024-11-30 13:13:45 -0600

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,11 +1,9 @@
-// battle.js
-
-class Battle {
+export class Battle {
     constructor(player, enemy) {
         this.player = player;
         this.enemy = enemy;
+        this.currentTurn = "player";
         this.battleLog = [];
-        this.currentTurn = "player"; // Tracks whose turn it is
     }
 
     rollDice(sides) {
@@ -13,40 +11,41 @@ class Battle {
     }
 
     playerAttack() {
-        const attackRoll = this.rollDice(20); // Roll a d20 for attack
-        if (attackRoll > this.enemy.armorClass) {
-            const damage = this.rollDice(this.player.weapon.damageDice);
+        const roll = this.rollDice(20);
+        const attackRoll = roll + this.player.stats.strength;
+        this.battleLog.push(`Player rolls ${roll} + Strength (${this.player.stats.strength}) = ${attackRoll}`);
+        if (attackRoll >= this.enemy.armorClass) {
+            const damage = this.rollDice(6); // Default weapon damage
             this.enemy.currentHP -= damage;
             this.battleLog.push(`You hit the ${this.enemy.name} for ${damage} damage!`);
         } else {
-            this.battleLog.push(`You missed the ${this.enemy.name}!`);
+            this.battleLog.push("You missed!");
         }
-        this.currentTurn = "enemy"; // Switch turn
+        this.currentTurn = "enemy";
     }
 
     enemyAttack() {
-        const attackRoll = this.rollDice(20); // Roll a d20 for attack
-        if (attackRoll > this.player.armorClass) {
-            const damage = this.rollDice(this.enemy.weapon.damageDice);
+        const roll = this.rollDice(20);
+        const attackRoll = roll + this.enemy.stats.strength;
+        this.battleLog.push(`Enemy rolls ${roll} + Strength (${this.enemy.stats.strength}) = ${attackRoll}`);
+        if (attackRoll >= this.player.armorClass) {
+            const damage = this.rollDice(6); // Default enemy damage
             this.player.currentHP -= damage;
             this.battleLog.push(`The ${this.enemy.name} hits you for ${damage} damage!`);
         } else {
-            this.battleLog.push(`The ${this.enemy.name} missed you!`);
+            this.battleLog.push(`The ${this.enemy.name} missed!`);
         }
-        this.currentTurn = "player"; // Switch turn
+        this.currentTurn = "player";
     }
 
     isBattleOver() {
         if (this.player.currentHP <= 0) {
-            this.battleLog.push("You have been defeated!");
+            this.battleLog.push("You have been defeated...");
             return true;
-        }
-        if (this.enemy.currentHP <= 0) {
+        } else if (this.enemy.currentHP <= 0) {
             this.battleLog.push(`You have defeated the ${this.enemy.name}!`);
             return true;
         }
         return false;
     }
 }
-
-export { Battle };
